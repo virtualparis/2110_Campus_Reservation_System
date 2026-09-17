7aed57df5bec68853c8c3b33196d5222bc93e796
Paris <230484764+virtualparis@users.noreply.github.com>
2026-09-17 14:10:05 -0500

    Implement user menu for resource reservation system
    Added a menu for user interaction with options for resource management.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include "ReservationManager.h"
+using namespace std;
 
 int main() {
     ReservationManager manager;
@@ -11,6 +12,22 @@ int main() {
     manager.loadResources("resources.txt");
 
     // menu with prompts for user
-    
+    int choice;
+    do {
+        cout << "==== Campus Resource Reservation System ====\n";
+        cout << "1. View Resources\n";
+        cout << "2. Create Reservation\n";
+        cout << "3. Cancel Reservation\n";
+        cout << "4. View Waiting Lists\n";
+        cout << "5. Undo Cancellation\n";
+        cout << "6. Search Reservations\n";
+        cout << "7. Sort Resources\n";
+        cout << "8. Generate Report\n";
+        cout << "9. Exit\n";
+        cout << "Enter Choice: ";
+        cin >> choice;
+    }
 
+
+    return 0;
 }
