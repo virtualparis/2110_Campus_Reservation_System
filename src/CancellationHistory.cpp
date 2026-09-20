b8e6a29faff2dfc60ebf914f7cd4a41a93d2975a
Romankatuwal47 <143305504+Romankatuwal47@users.noreply.github.com>
2026-09-20 16:17:58 -0500

    Update CancellationHistory.cpp

--- src/CancellationHistory.cpp
+++ src/CancellationHistory.cpp
@@ -1,15 +1,12 @@
 #include "../include/CancellationHistory.h"
 #include <iostream>
 
-// Save a cancelled reservation on top of the stack.
 void CancellationHistory::addCancelledReservation(
     const Reservation& reservation) {
 
     cancelledReservations.push(reservation);
 }
 
-// Return and remove the most recently cancelled reservation.
-// The reservation manager must restore it to active reservations.
 bool CancellationHistory::undoCancellation(
     Reservation& reservation) {
 
@@ -22,7 +19,6 @@ bool CancellationHistory::undoCancellation(
     return true;
 }
 
-// Display newest cancellations first without changing the stack.
 void CancellationHistory::displayCancellationHistory() const {
     if (cancelledReservations.empty()) {
         std::cout << "Cancellation history is empty.\n";
